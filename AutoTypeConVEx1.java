dd29054490c7dba2d986ba3c3e8255dba9325eb8
Pranavk1105 <pranavkale2806@gmail.com>
2025-01-06 15:30:27 +0530

    Add files via upload

---
+++ AutoTypeConVEx1.java
@@ -0,0 +1,13 @@
+class AutoTypeConVEx1 {
+    public static void main (String [] args)
+    {
+        int i =100;
+        long l = i;
+
+        float f = l;
+
+        System.out.println("Int value" + i);
+        System.out.println("Long value" + i);
+        System.out.println("Float value" + i);
+    }
+}
